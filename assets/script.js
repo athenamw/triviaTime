8098f45c332ab185b8418c998a31e580ee979663
Athena <wallisathena@gmail.com>
2023-05-19 17:03:10 -0700

    added reset css

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var gameBoard = document.getElementById("gameBoard");
+var beginButton = document.getElementById("button")
 
 document.getElementById("button").addEventListener("click", function(){
     gameBoard.style.display = "block";
-    
+    beginButton.style.display = "none";
 });
