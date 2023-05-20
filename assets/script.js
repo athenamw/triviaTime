7f2ee6ccc4644f39b19cb16df76568cac43b1e5f
Athena <wallisathena@gmail.com>
2023-05-19 21:45:11 -0700

    added 4 buttons

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,33 @@
 var gameBoard = document.getElementById("gameBoard");
 var beginButton = document.getElementById("button")
 
-document.getElementById("button").addEventListener("click", function(){
+
+document.getElementById("button").addEventListener("click", function () {
     gameBoard.style.display = "block";
     beginButton.style.display = "none";
 });
+
+function nextQuestion() {
+    gameBoard.style.display = "none";
+    
+}
+
+function showQuestion(question){
+    
+}
+
+function selectAnswer() {
+
+}
+
+const question = [
+    {
+        question: "What does % sign signify in javascript?",
+        answer: [
+            { text: "Division", correct: false },
+            { text: "Percentage", correct: false },
+            { text: "Remainder", correct: true },
+            { text: "Output", correct: false },
+        ]
+    }
+]
