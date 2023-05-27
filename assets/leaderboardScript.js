e96295b036c4db95770bf0dfb3341d4fe2742290
Athena <wallisathena@gmail.com>
2023-05-27 16:28:24 -0700

    sort highscores

--- assets/leaderboardScript.js
+++ assets/leaderboardScript.js
@@ -19,21 +19,28 @@ clear.addEventListener("click", function () {
 window.addEventListener("load", function () {
 
     var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-    for (let index = 0; index < highScores.length; index++) {
-        var names = highScores[index].name;
-        var time = highScores[index].time;
-        var row = document.createElement('tr');
-        var number = document.createElement('th');
-        number.textContent = index + 1;
-        number.setAttribute("scope", "row");
-        var winners = document.createElement('td');
-        winners.textContent = names;
-        var timer = document.createElement('td');
-        timer.textContent = time;
-        row.appendChild(number);
-        row.appendChild(winners);
-        row.appendChild(timer);
-        tableBody.appendChild(row);
-        
+    if (highScores.length > 0) {
+        // sort high scores
+        highScores.sort(function (a, b) {
+            return parseInt(a.time) - parseInt(b.time);
+        });
+
+        for (let index = 0; index < highScores.length; index++) {
+            var names = highScores[index].name;
+            var time = highScores[index].time;
+            var row = document.createElement('tr');
+            var number = document.createElement('th');
+            number.textContent = index + 1;
+            number.setAttribute("scope", "row");
+            var winners = document.createElement('td');
+            winners.textContent = names;
+            var timer = document.createElement('td');
+            timer.textContent = time;
+            row.appendChild(number);
+            row.appendChild(winners);
+            row.appendChild(timer);
+            tableBody.appendChild(row);
+
+        };
     };
 });
